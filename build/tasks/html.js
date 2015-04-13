8b72116df1cd5163051352559fb04569a1c9eb21
Tristan Smith <darcnite3000@gmail.com>
2015-04-13 16:24:23 +1000

    start using templatecache

--- build/tasks/html.js
+++ build/tasks/html.js
@@ -1,10 +1,20 @@
-var gulp          = require('gulp'),
-    changed       = require('gulp-changed'),
-    paths         = require('../paths');
+var gulp      = require('gulp'),
+    changed   = require('gulp-changed'),
+    concat    = require('gulp-concat'),
+    html2js   = require('gulp-html2js'),
+    paths     = require('../paths');
 
 
 gulp.task('html', function() {
   return gulp.src(paths.html.source)
     .pipe(changed(paths.html.output,{extension: '.html'}))
-    .pipe(gulp.dest(paths.html.output));
+    .pipe(html2js({
+    	outputModuleName: 'templates',
+    	base: paths.root,
+    	rename: function(moduleName){
+    		return ''+moduleName.replace('app/', './components/')
+    	}
+    }))
+    .pipe(concat('template.js'))
+    .pipe(gulp.dest(paths.html.output+'app/'));
 });
--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
 import {AppController} from './app';
 import {WelcomeController} from './welcome/welcome'
+import './template';
 
 import 'angular-new-router';
 
-angular.module('routable', ['ngNewRouter'])
+angular.module('routable', ['ngNewRouter','templates'])
 	.controller('AppController', AppController)
 	.controller('WelcomeController', WelcomeController)
